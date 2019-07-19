05ab13a73813d1643d4bfb61adfd5554656a0126
=Amy <AmandaZheng@xingren.com>
2019-07-19 11:23:41 +0800

    scala

---
+++ src/main/scala/com/ecut/HelloWorldTest.scala
@@ -0,0 +1,14 @@
+package com.ecut
+
+/**
+  * @author Amy
+  * @date 2019-07-19 10:47
+  */
+object HelloWorldTest {
+
+  def main(args: Array[String]): Unit = {
+    var s: String = "Hello World !";
+    println(s);
+  }
+
+}
